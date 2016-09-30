c2d1b8130e942495e0b0477578ae7072df47912f
Adam Le Doux <aledoux6@gatech.edu>
2016-09-29 18:53:11 -0700

    font experiment v1 feature complete + sysfont

---
+++ VexLetterer/src/Font.hx
@@ -0,0 +1,76 @@
+import vexlib.Vex;
+import vexlib.VexPropertyInterface;
+
+typedef FontFormat = {
+	@:optional public var type : Property;
+	@:optional public var id : Property;
+	@:optional public var characters : Array<VexJsonFormat>;
+}
+
+class Font {
+	public var type : Null<Property>;
+	public var id : Null<Property>;
+
+	//map of character strings to vex drawing data
+	private var characters : Map<String,VexJsonFormat> = new Map<String,VexJsonFormat>(); //does it need to be private?
+	
+	public function new(?json:FontFormat) {
+		type = "font"; //this is the same no matter what
+		if (json != null) {
+			deserialize(json);
+		}
+	}
+
+	public function deserialize(json:FontFormat) {
+		if (json.id != null) id = json.id;
+
+		if (json.characters != null) {
+			for (c in json.characters) {
+				if (c.id != null) { //requires an id to add it to the map
+					set(c.id, c);
+				}
+			}
+		}
+	}
+
+	public function serialize() : FontFormat {
+		var json : FontFormat = {};
+
+		json.type = type;
+		if (id != null) json.id = id;
+
+		json.characters = [];
+		for (c in alphabeticalCharacterKeys()) {
+			json.characters.push( get(c) );
+		}
+
+		return json;
+	}
+
+	public function exists(char:String) : Bool {
+		return characters.exists(char);
+	}
+
+	public function get(char:String) : VexJsonFormat {
+		return characters[char];
+	}
+
+	public function set(char:String, vex:VexJsonFormat) {
+		characters[char] = vex;
+	}
+
+	public function alphabeticalCharacterKeys() : Array<String> {
+		var keyArr = [];
+		for (k in characters.keys()) {
+			keyArr.push(k);
+		}
+		keyArr.sort( function(a:String, b:String):Int
+		{
+			if (a < b) return -1;
+			if (a > b) return 1;
+			return 0;
+		} );
+		return keyArr;
+	}
+
+}
--- VexLetterer/src/Main.hx
+++ VexLetterer/src/Main.hx
@@ -1,19 +1,35 @@
 
+//luxe
 import luxe.Input;
 import luxe.Vector;
 import luxe.Color;
 import luxe.tween.Actuate;
 
+//for vex
 import vexlib.Vex;
 import vexlib.VexPropertyInterface;
 
+//for save load
+import sys.io.File;
+import dialogs.Dialogs;
+import haxe.Json;
+
 /*
 	TODO
-	- instant replay
-	- font-view
-	- save font
-	- save characters
-	- switch font
+	X instant replay
+	X font-view
+	X save/load font
+	X save characters
+	X word wrap in typing test mode
+	- variable-width characters?
+	X edit characters that already exist
+	- don't save a weight?
+	- scrolling through letters?
+	X clear typing test
+	X default font
+	X new font
+	- escape without saving chars
+	- action message (like save)
 */
 
 enum EditorMode {
@@ -40,13 +56,15 @@ class Main extends luxe.Game {
 	var strokes : Array<Array<Vector>> = [];
 
 	//font storage
-	var fontMap : Map<String,VexJsonFormat> = new Map<String,VexJsonFormat>();
+	var font : Font = new Font();
+	var useDefaultFont = true;
+	var defaultFontPath = "/Users/adamrossledoux/Code/Haxe/AdventureGame/VexLetterer/assets/sysfont.vex";
 
 	//font view mode
 	var fontVex : Array<Vex> = [];
 
 	//typing test mode
-	var typingCount = 0;
+	var typingCount : Int = 0;
 	var typingVex : Array<Vex> = [];
 
 	override function ready() {
@@ -54,17 +72,34 @@ class Main extends luxe.Game {
 		Luxe.camera.pos.subtract(Luxe.screen.mid); //put 0,0 in the center of the camera
 		//TODO size mode not working???
 
+		//load default font
+		if (useDefaultFont) {
+			var fileStr = File.getContent(defaultFontPath);
+			var json = Json.parse(fileStr);
+			font = new Font(json);
+		}
+
+		switchMode(EditorMode.FontView);
+
 	} //ready
 
 	override function onkeyup( e:KeyEvent ) {
 
-		if(e.keycode == Key.escape) {
-			Luxe.shutdown();
-		}
-
 	} //onkeyup
 
 	override function onkeydown( e:KeyEvent ) {
+
+		//refactor so it fits w/ other code
+		if(e.keycode == Key.escape) {
+			if (mode == EditorMode.CharEdit) {
+				//go back to font view w/o saving the char work
+				switchMode(EditorMode.FontView);
+			}
+			else {
+				//usually quit
+				Luxe.shutdown();	
+			}
+		}
 
 		/*
 		//hack
@@ -100,7 +135,6 @@ class Main extends luxe.Game {
 				if (strokes.length > 0) {
 					saveChar(curChar, strokes);
 				}
-				//mode = EditorMode.FontView;
 				switchMode(EditorMode.FontView);
 			}
 		}
@@ -108,10 +142,63 @@ class Main extends luxe.Game {
 			if (e.keycode == Key.tab) {
 				switchMode(EditorMode.TypingTest);
 			}
+
+			//open
+			if (e.keycode == Key.key_o && e.mod.meta ) {
+				//load file
+				var path = Dialogs.open("Open dialog");
+				var fileStr = File.getContent(path);
+				var json = Json.parse(fileStr);
+
+				font = new Font(json);
+				switchMode(EditorMode.FontView); //reload font
+			}
+
+			//save
+			if (e.keycode == Key.key_s && (e.mod.meta || e.mod.alt) ) {
+				//get path & open file
+				var path = "";
+
+				if (e.mod.meta) {
+					//if not holding shift, you have to choose the file
+					path = Dialogs.save("Save dialog");
+				}
+				else if (e.mod.alt) {
+					path = defaultFontPath;
+				}
+				else {
+					//you're screwed
+				}
+
+				try {
+					var output = File.write(path);
+
+					//get data & write it
+					var saveJson = font.serialize();
+					var saveStr = Json.stringify(saveJson, null, "	");
+					output.writeString(saveStr);
+
+					//close file
+					output.close();
+					trace("saved! " + path);
+				}
+				catch (e:Dynamic){
+					trace("error saving: " + e);
+				}
+			}
+
+			//new
+			if (e.keycode == Key.key_n && e.mod.meta) {
+				font = new Font();
+				switchMode(EditorMode.FontView); //clear font view
+			}
 		}
 		else if (mode == EditorMode.TypingTest) {
 			if (e.keycode == Key.tab) {
 				switchMode(EditorMode.FontView);
+			}
+			if (e.keycode == Key.backspace) {
+				switchMode(EditorMode.TypingTest); //clear typing test by switching modes
 			}
 		}
 	}
@@ -227,6 +314,12 @@ class Main extends luxe.Game {
 	}
 
 	override function ontextinput(e:TextEvent) {
+		if ( Luxe.input.keydown( Key.lmeta ) || Luxe.input.keydown( Key.rmeta ) || 
+				Luxe.input.keydown( Key.lalt ) || Luxe.input.keydown( Key.ralt )
+					) {
+			return; //skip this if doing a command	
+		} 
+
 		var nextChar = e.text.charAt(0); //grab first char as current char
 		if (mode == EditorMode.FontView || mode == EditorMode.CharEdit) {
 			if (curChar != nextChar) {
@@ -248,27 +341,31 @@ class Main extends luxe.Game {
 		else if (mode == EditorMode.TypingTest) {
 			var scale = 0.1; //duped code
 			var topLeft = Luxe.camera.pos.clone().add(new Vector(300 * scale * 0.5, 550 * scale * 0.5));
-			if (fontMap.exists(nextChar)) {
-				var v = new Vex(fontMap[nextChar]);
-				v.pos = topLeft.clone().add( new Vector(300 * scale * typingCount, 0) );
+
+			if (font.exists(nextChar)) {
+				var row : Int = cast(typingCount / 20); //arbitary line width
+				var col : Int = typingCount - (row * 20);
+
+				var v = new Vex(font.get(nextChar));
+				v.pos = topLeft.clone().add( new Vector(300 * scale * col, 550 * scale * row) );
 				v.scale = new Vector(scale, scale, scale);
 				typingVex.push(v);
 				animateStrokes(v,0.3);
 			}
-			else {
-				//ignore undefined characters
-			}
+
 			typingCount++;
 		}
 	}
 
 	function saveChar(char, strokes) {
-		fontMap[char] = {
+		font.set(char, 
+			{
 				id: char,
 				type: "line",
 				path: strokes,
 				weight: "10" //testing
-			};
+			}
+		);
 	}
 
 	function drawFont() {
@@ -277,22 +374,11 @@ class Main extends luxe.Game {
 		var vexCharacters = [];
 		var i = 0;
 
-		var keyArr = [];
-		for (k in fontMap.keys()) {
-			keyArr.push(k);
-		}
-		keyArr.sort( function(a:String, b:String):Int
-		{
-			//a = a.toLowerCase();
-			//b = b.toLowerCase();
-			if (a < b) return -1;
-			if (a > b) return 1;
-			return 0;
-		} );
-
-		for (k in keyArr) {
-			var v = new Vex(fontMap[k]);
-			v.pos = topLeft.clone().add( new Vector(300 * scale * i, 0) );
+		for (k in font.alphabeticalCharacterKeys()) {
+			var row : Int = cast(i / 20); //arbitary line width
+			var col : Int = i - (row * 20);
+			var v = new Vex( font.get(k) );
+			v.pos = topLeft.clone().add( new Vector(300 * scale * col, 550 * scale * row) );
 			v.scale = new Vector(scale, scale, scale);
 			vexCharacters.push(v);
 			i++;
@@ -358,7 +444,13 @@ class Main extends luxe.Game {
 		else if (nextMode == EditorMode.CharEdit) {
 			//clear strokes
 			curStroke = [];
-			strokes = [];
+			if (font.exists(curChar)) {
+				var pathProp : Property = font.get(curChar).path;
+				strokes = pathProp;
+			}
+			else {
+				strokes = [];
+			}
 		}
 		else if (nextMode == EditorMode.TypingTest) {
 			typingVex = [];
